43d1ca00d85155c5c2a5a17faee87ee3f0e3449a
Guillermo Sequeira <guillermo.sequeira90@gmail.com>
2024-09-06 09:14:29 -0600

    Populate fallback podcasts

--- src/utils/fallbackPodcasts.ts
+++ src/utils/fallbackPodcasts.ts
@@ -7,6 +7,15 @@ const rssFeedUrls = [
   "https://feeds.megaphone.fm/hubermanlab",
   "https://lexfridman.com/feed/podcast/",
   "https://feeds.megaphone.fm/thediaryofaceo",
+  "https://rss.art19.com/business-wars",
+  "https://feeds.megaphone.fm/investlikethebest",
+  "https://feeds.megaphone.fm/ATHLLC5883700320",
+  "https://feeds.simplecast.com/3hnxp7yk",
+  "https://rss.art19.com/masters-of-scale",
+  "https://feeds.simplecast.com/54nAGcIl",
+  "https://feeds.megaphone.fm/thispastweekend",
+  "https://omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/A91018A4-EA4F-4130-BF55-AE270180C327/44710ECC-10BB-48D1-93C7-AE270180C33E/podcast.rss",
+
   // Add more RSS feed URLs here to reach a total of 12
 ];
 
